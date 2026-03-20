4e96ca6456e868e4c3e1a358d1865214d2ce06e7
Lachlan Fisher <lachiefish@gmail.com>
2026-03-20 19:58:12 +1100

    Refactor FTP server code by removing unused variables and simplifying update logic

--- include/ftp_server.h
+++ include/ftp_server.h
@@ -1,7 +1,5 @@
 #pragma once
 
-#define FTP_SERVER_NETWORK_TYPE DEFAULT_FTP_SERVER_NETWORK_TYPE_ESP32
-#define STORAGE_TYPE STORAGE_SD_MMC
 #include <SimpleFTPServer.h>
 
 class FTPServer
@@ -12,5 +10,4 @@ public:
 
 private:
   FtpServer ftp_server_instance;
-  bool started = false;
 };
--- src/ftp_server.cpp
+++ src/ftp_server.cpp
@@ -5,7 +5,6 @@
 void FTPServer::begin()
 {
   ftp_server_instance.begin(FTP_USERNAME, FTP_PASSWORD);
-  started = true;
 
   Serial.println(F("[FTP] FTP server started"));
   Serial.print(F("[FTP] Username: "));
@@ -14,15 +13,5 @@ void FTPServer::begin()
 
 void FTPServer::update()
 {
-  if (!started)
-  {
-    return;
-  }
-
-  if (WiFi.softAPgetStationNum() <= 0)
-  {
-    return;
-  }
-
   ftp_server_instance.handleFTP();
 }
