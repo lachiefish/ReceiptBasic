456e41e668eb5a481649320b45a4fac0bfae4167
Lachlan Fisher <lachiefish@gmail.com>
2026-03-20 19:12:31 +1100

    Refactor FTP server implementation by moving definitions to header and removing unnecessary includes

--- include/ftp_server.h
+++ include/ftp_server.h
@@ -1,5 +1,9 @@
 #pragma once
 
+#define FTP_SERVER_NETWORK_TYPE DEFAULT_FTP_SERVER_NETWORK_TYPE_ESP32
+#define STORAGE_TYPE STORAGE_SD_MMC
+#include <SimpleFTPServer.h>
+
 class FTPServer
 {
 public:
@@ -7,5 +11,6 @@ public:
   void update();
 
 private:
+  FtpServer ftp_server_instance;
   bool started = false;
 };
--- src/ftp_server.cpp
+++ src/ftp_server.cpp
@@ -1,18 +1,5 @@
 #include "ftp_server.h"
-
-#include <Arduino.h>
-#include <WiFi.h>
-
 #include "config.h"
-
-#define FTP_SERVER_NETWORK_TYPE DEFAULT_FTP_SERVER_NETWORK_TYPE_ESP32
-#define STORAGE_TYPE STORAGE_SD_MMC
-#include <SimpleFTPServer.h>
-
-namespace
-{
-  FtpServer ftp_server_instance;
-}
 
 void FTPServer::begin()
 {
@@ -27,11 +14,6 @@ void FTPServer::begin()
 void FTPServer::update()
 {
   if (!started)
-  {
-    return;
-  }
-
-  if (WiFi.softAPgetStationNum() <= 0)
   {
     return;
   }
